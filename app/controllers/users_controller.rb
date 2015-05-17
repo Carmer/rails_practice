ada8edd0b280e60fcd3b0e67cff21728f8e7e711
Carmer <carmerandrew@gmail.com>
2015-05-17 14:21:28 -0600

    microposts and picture upload with AWS

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by(id: params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def create
@@ -54,16 +55,6 @@ class UsersController < ApplicationController
     end
 
     # Before filters
-
-   # Confirms a logged-in user.
-
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
 
     # Confirms the correct user.
 
